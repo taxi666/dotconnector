9a835be449f29518192d256e849adee9e4e4d90a
Zhu Ted <zh_wf@163.com>
2013-09-09 18:08:15 +0800

    html基本完成 03.09.09

---
+++ backup.html
@@ -0,0 +1,8 @@
+<div  class="ft_bottom">
+	<ul class="share_list">
+		<li><a href="http://www.facebook.com/wix" class="share_facebook" target="_blank"></a></li>
+		<li><a href="https://www.twitter.com/wix" class="share_twitter" target="_blank"></a></li>
+		<li><a href="https://plus.google.com/117167403531518744294" class="share_google" target="_blank"></a></li>
+		<li><a href="http://www.linkedin.com/company/wix.com?trk=tabs_biz_home" class="share_linkedin" target="_blank"></a></li>
+	</ul>
+</div> 
--- js/pri_index.js
+++ js/pri_index.js
@@ -1,24 +1,43 @@
 $(document).ready(function(){
-	var i = 0;
+	var i=0,j=0;
+	//=S ad top
 	var ad_top_list = $("#ad_top_list");
 	setInterval(function (){
 	ad_top_list.find("li").eq(i).fadeOut(1000);
-	ad_top_list.find("li").eq((++i)%6).fadeIn(1000);
-	$("#ad_top_num").text(i+1);	
+	i++;
 	if(i>5) i=0;
+	ad_top_list.find("li").eq(i).fadeIn(1000);
+	$("#ad_top_num").text(i+1);
 	},3000);
+	//=E ad top
 
+	//=S ad middle
+	$("#icon_ad_mid_left").click(function(){
+		if(mar_ad_bottom)clearInterval(mar_ad_bottom);
+		$("#ad_bottom_switch").addClass("switch_off").removeClass("switch_on");
+		ad_bottom_list.find("li").eq(j).fadeOut(1000);
+		j--;
+		if(j<0) j=2;
+		ad_bottom_list.find("li").eq(j).fadeIn(1000);
+		$("#ad_bottom_num").text(j+1);
+		
+	});
+	$("#icon_ad_mid_right").click(function(){
+		if(mar_ad_bottom)clearInterval(mar_ad_bottom);
+		$("#ad_bottom_switch").addClass("switch_off").removeClass("switch_on");
+		ad_bottom_list.find("li").eq(j).fadeOut(1000);
+		j++;
+		if(j>2) j=0;
+		ad_bottom_list.find("li").eq(j).fadeIn(1000);
+		$("#ad_bottom_num").text(j+1);
+		
+	});
 	var speed=10;
 	var demo1=$("#ad_mid_list");
 	var demo2=$("#ad_mid_list2");
 	var demo=$("#ad_mid_main");
 	var MyMar;
 	demo2.html(demo1.html());
-	//alert(demo.class);
-	//alert(document.getElementById("ad_mid_main").offsetWidth);
-	//alert("demo.offsetWidth:"+demo.get(0).offsetWidth);
-	// alert("demo.offsetWidth:"+demo.offsetWidth);
-	// alert("demo1.offsetWidth:"+demo1.offsetWidth);
 	function marquee_left(){
 		if(demo1.width() <= demo.scrollLeft())
 		demo.scrollLeft(0);
@@ -46,5 +65,34 @@ $(document).ready(function(){
 	$("#icon_ad_mid_left").mouseout(function(){
 		clearInterval(MyMar);
 	});
+	//=E ad middle
+
+	//=S ad bottom
+	var ad_bottom_list = $("#ad_bottom_list");
+	function marqueeAdBottom() {
+		ad_bottom_list.find("li").eq(j).fadeOut(1000);
+		j++;
+		if(j>=3) j=0;
+		ad_bottom_list.find("li").eq(j).fadeIn(1000);
+		$("#ad_bottom_num").text(j+1);			
+	}
+	var mar_ad_bottom = setInterval(marqueeAdBottom,3000);
+	$("#ad_bottom_switch").click(function(){
+		if($("#ad_bottom_switch").hasClass('switch_on')){
+			clearInterval(mar_ad_bottom);
+			$("#ad_bottom_switch").addClass("switch_off").removeClass("switch_on");
+		}
+		else {
+			mar_ad_bottom = setInterval(marqueeAdBottom,3000);
+			$("#ad_bottom_switch").addClass("switch_on").removeClass("switch_off");
+		}
+	});
+	//=E ad bottom
+	$("#logo_show_more").click(
+		function(){
+		$("#log_list2").fadeIn(1000);
+		$("#logo_show_more").hide();
+	});
+	
 });
 
